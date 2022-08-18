c894489c42fd03944aa2a013d628c126e16a2ba8
nahuel utn <nahuel.utn.frc.piguillem@gmail.com>
2022-08-17 21:01:13 -0300

    Basica app

--- src/models.nim
+++ src/models.nim
@@ -10,5 +10,8 @@ proc show*(r:Riesgo):string=
     var res="{id:" & $r.id & " , nombre: " & $r.nombre & "}"
     return res
 
+proc inv_cmp_risk*(r1,r2:Riesgo):int=
+    -1*cmp(r1.exposicion(),r2.exposicion())
+
 proc cmp_risk*(r1,r2:Riesgo):int=
     cmp(r1.exposicion(),r2.exposicion())
--- src/riesgos.nim
+++ src/riesgos.nim
@@ -1,34 +1,39 @@
 import ioutiles
+import std/algorithm
+import std/strutils
 import models
 import std/os
 
-proc mergear():seq[Riesgo]=
-  return @[Riesgo()]
+iterator `...`*[T](a: T, b: T): T =
+  var res: T = T(a)
+  while res <= b:
+    yield res
+    inc res
 
-proc merge_sort(ls: seq[Riesgo]):seq[Riesgo]=
-  return @[Riesgo()]
-
-
-proc ordenar_riesgos():seq[Riesgo]=
-  
-  
-  return @[Riesgo()]
 
 proc ids_x_porc(porc: float, rs: var seq[Riesgo]):seq[int]=
-  var acumulado=0
-  for r in rs:
-    acumulado+=r.exposicion()
+  var exposicion_total=0
+  rs.sort(inv_cmp_risk)
   
-
-  return @[1]
+  var ids=newSeq[int](0)
+  for r in rs:
+    exposicion_total+=r.exposicion()
+  var acumulado=0.0
+  var i=0
+  while acumulado<porc:
+    acumulado+=rs[i].exposicion()/exposicion_total
+    ids.add(rs[i].id)
+    inc i
+  return ids
 
 when isMainModule:
   var opcion="0.7"
   when declared(commandLineParams):
     if(len(commandLineParams())>0):
       opcion=commandLineParams()[0]
-  echo opcion
-  let rs=read_archivo("riesgos")
-  for r in rs:
-    echo r.show()
-  
+  let fopcion=parseFloat(opcion)
+  var rs=read_archivo("riesgos")
+  var ids=ids_x_porc(fopcion,rs)
+  for i in 0...len(ids)-1:
+    echo rs[i].show()
+    
